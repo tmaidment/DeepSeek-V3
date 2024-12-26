6cdb2ecf03694c2333bdbfbd142507778d4b1f30
Tristan Maidment <email@theedman.com>
2024-12-26 18:43:35 -0500

    increase cpu count

--- inference/generate.py
+++ inference/generate.py
@@ -76,7 +76,7 @@ def main(
     if rank != 0:
         print = lambda *_, **__: None
     torch.set_default_dtype(torch.bfloat16)
-    torch.set_num_threads(8)
+    torch.set_num_threads(os.cpu_count())
     torch.manual_seed(965)
     with open(config) as f:
         args = ModelArgs(**json.load(f))
